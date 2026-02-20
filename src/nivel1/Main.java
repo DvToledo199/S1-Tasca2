a31d8ccc624c7db872e8105e93f42ec7045a6715
David Toledo <toledo_1990@hotmail.com>
2026-02-20 21:30:50 +0100

    feat: complete level 1 exception handling with custom and index errors

--- src/nivel1/Main.java
+++ src/nivel1/Main.java
@@ -5,6 +5,19 @@ public class Main {
 
         Sale s1 = new Sale();
 
-        s1.calculateTotal();
+// Primero provocamos EmptySaleException
+        try {
+            s1.calculateTotal();
+        } catch (EmptySaleException e) {
+            System.out.println(e.getMessage());
+        }
+
+// Luego provocamos IndexOutOfBoundsException
+        try {
+            java.util.ArrayList<String> list = new java.util.ArrayList<>();
+            System.out.println(list.get(5));
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
